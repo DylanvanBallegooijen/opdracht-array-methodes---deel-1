8f07718f3942ff5a132fa0a64da666b9048c34f7
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-09-28 15:59:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//const addTheWordCool = ["nice", "awesome", "tof"];
+//addTheWordCool.push ("cool");
+// add your code
+
+//console.log(addTheWordCool);
+//resultaat: ["nice", "awesome", "tof", "cool"]
+
+//const amountOfElementInArray = ["appel", "peren", "citroenen"];
+//amountOfElementInArray.length();
+
+//console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+// 3
+
+/*  const selectBelgiumFromBenelux = (["Belgie", "Nederland", "Luxemburg"]);
+selectBelgiumFromBenelux;  
+
+console.log(selectBelgiumFromBenelux[0]);
+
+const lastElementInArray = ["haas", "cavia", "Kip", "schildpad"];
+lastElementInArray;
+
+console.log(lastElementInArray[lastElementInArray.length -1]);*/
+
+//const impeachTrumpSlice = ["Trump", "Obama", "Bush", "Clinton"];
+//impeachTrumpSlice.splice(0, 1,);
+
+//console.log(impeachTrumpSlice);
+ // ["Obama", "Bush", "Clinton"] 
+
+// const stringsTogether = ['Winc', 'Academy', 'is', 'leuk', ';-}'];
+ //   stringsTogether
+
+//console.log(stringsTogether.join(' ')); 
+  //resultaat: "Winc Academy is leuk ;-}"
+
+  
+  
+  
+
+  combineArrays([1,2,3], [4,5,6]) 
+  // resultaat: [1,2,3,4,5,6]
